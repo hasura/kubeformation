3c23fcd38598acad2ee4ac145594bd19aa367874
shahidhk <shahidh@hasura.io>
2018-04-18 11:43:27 +0530

    refactor kubeformation cmd

--- pkg/cmd/root.go
+++ pkg/cmd/root.go
@@ -3,20 +3,16 @@ package cmd
 import (
 	"io/ioutil"
 	"os"
+	"path/filepath"
 
-	prv "github.com/hasura/kubeformation/pkg/provider"
+	"github.com/hasura/kubeformation/pkg/provider"
 	"github.com/hasura/kubeformation/pkg/spec"
 	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 
+	// Register v1 spec handler
 	_ "github.com/hasura/kubeformation/pkg/spec/v1"
-)
-
-var (
-	file      string
-	provider  string
-	outputDir string
 )
 
 var ErrInvalidUsage = errors.New("kubeformation: invalid command usage")
@@ -26,45 +22,124 @@ var rootCmd = &cobra.Command{
 	Short:         "Kubeformation can bootstrap your cloud provider specific template for Kubernetes",
 	SilenceUsage:  true,
 	SilenceErrors: true,
-	RunE: func(cmd *cobra.Command, args []string) error {
-		var data []byte
-		var err error
-		if file == "-" {
-			// read from stdin
-			data, err = ioutil.ReadAll(os.Stdin)
-			if err != nil {
-				return err
-			}
-		} else {
-			// read from file
-			data, err = ioutil.ReadFile(file)
-			if err != nil {
-				return err
-			}
-		}
-		handler, err := spec.Read(data)
-		if err != nil {
-			return err
-		}
-		log.Info(handler.GetVersion())
-		files, err := handler.GenerateProviderTemplate(prv.NOP)
-		if err != nil {
-			return err
-		}
-		for k, v := range files {
-			log.Printf("%s\n%s\n\n", k, string(v))
-		}
-		return nil
-	},
+	RunE:          runKubeformation,
 }
 
+var kfm Kubeformation
+
 func init() {
-	rootCmd.Flags().StringVarP(&file, "file", "f", "", "cluster spec file to read (- to read from stdin)")
+	rootCmd.Flags().StringVarP(&kfm.InputFile, "file", "f", "", "cluster spec file to read (- to read from stdin)")
 	rootCmd.MarkFlagRequired("file")
-	rootCmd.Flags().StringVarP(&provider, "provider", "p", "", "managed kubernetes provider for which template has to be bootstrapped")
-	rootCmd.Flags().StringVarP(&outputDir, "output", "o", "", "output directory to write templates")
+	rootCmd.Flags().StringVarP(&kfm.ProviderValue, "provider", "p", "", "managed kubernetes provider for which template has to be bootstrapped")
+	rootCmd.Flags().StringVarP(&kfm.OutputDir, "output", "o", "", "output directory to write templates")
+	rootCmd.MarkFlagRequired("output")
 }
 
 func Execute() error {
 	return rootCmd.Execute()
+}
+
+type Kubeformation struct {
+	// Input Flags
+	ProviderValue string
+	InputFile     string
+	OutputDir     string
+
+	// Processed Data
+	Data     []byte
+	Provider provider.ProviderType
+
+	// Rendered Files
+	OutputFiles map[string][]byte
+
+	// Handler to generate files
+	Handler spec.VersionedSpecHandler
+}
+
+func (k *Kubeformation) ParseInputFlags() error {
+	var err error
+	if k.InputFile == "-" {
+		// read from stdin
+		k.Data, err = ioutil.ReadAll(os.Stdin)
+		if err != nil {
+			return err
+		}
+	} else {
+		// read from file
+		k.Data, err = ioutil.ReadFile(k.InputFile)
+		if err != nil {
+			return err
+		}
+	}
+
+	switch k.ProviderValue {
+	case "gke":
+		k.Provider = provider.GKE
+	case "aks":
+		k.Provider = provider.AKS
+	case "eks":
+		k.Provider = provider.EKS
+	default:
+		k.Provider = provider.NOP
+	}
+
+	return nil
+}
+
+func (k *Kubeformation) GetHandler() error {
+	handler, err := spec.Read(k.Data)
+	if err != nil {
+		return err
+	}
+	k.Handler = handler
+	return nil
+}
+
+func (k *Kubeformation) RenderOutputFiles() error {
+	var err error
+	k.OutputFiles, err = k.Handler.GenerateProviderTemplate(k.Provider)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (k *Kubeformation) WriteFilesToDir() error {
+	err := os.MkdirAll(k.OutputDir, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	for name, data := range k.OutputFiles {
+		err := ioutil.WriteFile(filepath.Join(k.OutputDir, name), data, 0644)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (k *Kubeformation) PrintFiles() {
+	for k, v := range k.OutputFiles {
+		log.Printf("%s\n%s\n\n", k, string(v))
+	}
+}
+
+func runKubeformation(cmd *cobra.Command, args []string) error {
+	err := kfm.ParseInputFlags()
+	if err != nil {
+		return err
+	}
+	err = kfm.GetHandler()
+	if err != nil {
+		return err
+	}
+	err = kfm.RenderOutputFiles()
+	if err != nil {
+		return err
+	}
+	err = kfm.WriteFilesToDir()
+	if err != nil {
+		return err
+	}
+	return nil
 }
