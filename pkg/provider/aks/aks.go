9fcc009f6fd8e713c6103650bac42cd4373092f8
shahidhk <shahidh@hasura.io>
2018-04-20 12:25:11 +0530

    add tests and comments for aks

--- pkg/provider/aks/aks.go
+++ pkg/provider/aks/aks.go
@@ -1,3 +1,12 @@
+// Package aks implements AKS provider for Kubeformation. It can generate Azure
+// Resource Manager templates in JSON format. Spec is the input struct.
+//
+// Azure Deployment Manager templates are written in JSON formatted files,
+// typically a resource definition file and a parameters file. Kubeformation
+// converts Spec into azuredeploy.json and azuredeploy.parameters.json files.
+// There could be more files depending on the presence of volumes/disks. These
+// files are defined as Go templates in template.go and are rendered with Spec
+// as the context.
 package aks
 
 import (
@@ -8,11 +17,17 @@ import (
 )
 
 const (
-	DefaultOSType     = "Linux"
+	// Default version of Kubernetes to use
 	DefaultK8SVersion = "1.8.1"
-	DefaultVMSize     = "Standard_D2_v2"
+
+	// Default Azure VM size
+	DefaultVMSize = "Standard_D2_v2"
+
+	// Default OS type, available options are Linux and Windows
+	DefaultOSType = "Linux"
 )
 
+// funcMap is a template helper function
 var funcMap = template.FuncMap{
 	"sub": func(i int) int {
 		if i == 0 {
@@ -22,48 +37,74 @@ var funcMap = template.FuncMap{
 	},
 }
 
+// Spec defines the context required to render ARM template.
 type Spec struct {
-	Name       string
+	// Name of the cluster
+	Name string
+
+	// Kubernetes version for the cluster
 	K8SVersion string
-	NodePools  []NodePool
-	Volumes    []Volume
+
+	// Node pools to be created
+	NodePools []NodePool
+
+	// Persistent disks to be created
+	Volumes []Volume
 }
 
+// NodePool defines a collection of nodes (VMs) with similar properties that can
+// be used by a Kubernetes cluster to schedule workloads.
 type NodePool struct {
-	Name   string
-	Count  int64
+	// Name of the node pool
+	Name string
+
+	// Number of nodes in this pool
+	Count int64
+
+	// Size (type) of the VMs
 	VMSize string
+
+	// Type of OS to use
 	OSType string
 }
 
+// Volume defines a Azure Data Disk that can persist data.
 type Volume struct {
-	Name   string
+	// Name of the disk
+	Name string
+
+	// Size of the disk in GB
 	SizeGB int
 }
 
+// NewDefaultSpec returns a spec object which is barely enough to render a valid
+// ARM template.
 func NewDefaultSpec() *Spec {
 	return &Spec{
 		Name:       "aks-cluster",
 		K8SVersion: DefaultK8SVersion,
 		NodePools: []NodePool{
 			NodePool{
-				Name:   "aks-cluster-np-1",
+				Name:   "np-1",
 				Count:  1,
 				OSType: DefaultOSType,
 				VMSize: DefaultVMSize,
 			},
 		},
-		Volumes: []Volume{},
 	}
 }
 
+// GetType returns the type of this provider.
 func (s *Spec) GetType() provider.ProviderType {
 	return provider.AKS
 }
 
+// MarshalFiles returns the rendered ARM template as a map which indicates
+// filename as keys and file content as value.
+// FIXME: test does not capture the template errors.
 func (s *Spec) MarshalFiles() (map[string][]byte, error) {
 	var adb bytes.Buffer
-	azureDeployJinjaTmpl, err := template.New("azureDeploy.json").Funcs(funcMap).Parse(azureDeployJinja)
+	azureDeployJinjaTmpl, err := template.New("azuredeploy.json").Funcs(funcMap).Parse(azureDeployJSON)
 	if err != nil {
 		return nil, err
 	}
@@ -82,7 +123,7 @@ func (s *Spec) MarshalFiles() (map[string][]byte, error) {
 		return nil, err
 	}
 
-	//FIXME: Create new template for managed disks.
+	// FIXME: Create new template for managed disks.
 
 	return map[string][]byte{
 		"azuredeploy.json":            adb.Bytes(),
--- pkg/provider/aks/aks_test.go
+++ pkg/provider/aks/aks_test.go
@@ -1,8 +1,32 @@
 package aks
 
-// azureDeployJSON is a raw Go template string used to render azuredeploy.json
-// file for Azure Resource Manager.
-var azureDeployJSON = `{
+import (
+	"reflect"
+	"testing"
+
+	"github.com/hasura/kubeformation/pkg/provider"
+)
+
+func TestGetType(t *testing.T) {
+	s := Spec{}
+	got := s.GetType()
+	if got != provider.AKS {
+		t.Fatalf("expected provider '%v', got '%v'", provider.AKS, got)
+	}
+}
+
+func TestMarshalFiles(t *testing.T) {
+	tt := []struct {
+		spec *Spec
+		name string
+		data map[string][]byte
+		err  error
+	}{
+		{
+			NewDefaultSpec(),
+			"valid default spec",
+			map[string][]byte{
+				"azuredeploy.json": []byte(`{
   "$schema": "https://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#",
   "contentVersion": "1.0.0.0",
   "parameters": {
@@ -47,34 +71,19 @@ var azureDeployJSON = `{
       "apiVersion": "2017-08-31",
       "type": "Microsoft.ContainerService/managedClusters",
       "location": "[resourceGroup().location]",
-      "name": "{{ .Name }}",
+      "name": "aks-cluster",
       "properties": {
         "dnsPrefix": "[parameters('dnsNamePrefix')]",
         "agentPoolProfiles": [
-          {{- $nodeLength := sub (len .NodePools) }}
-          {{- range $i, $node := .NodePools }}
           {
-            "name": "{{ $.Name }}-{{ .Name }}",
-            "count": {{ .Count }},
+            "name": "aks-cluster-np-1",
+            "count": 1,
             "dnsPrefix": "[variables('agentsEndpointDNSNamePrefix')]",
-            {{- if .VMSize }}
-            "vmSize": "{{ .VMSize }}",
-            {{- else }}
             "vmSize": "Standard_D2_v2",
-            {{- end -}}
-            {{- if .OSType }}
-            "osType": "{{ .OSType }}"
-            {{- else }}
             "osType": "Linux"
-            {{- end }}
-          {{- if ne $i $nodeLength }}
-          },
-          {{- else }}
           }
-          {{- end }}
-          {{- end }}
         ],
-        "kubernetesVersion": "{{ .K8SVersion }}",
+        "kubernetesVersion": "1.8.1",
         "linuxProfile": {
           "adminUsername": "[parameters('adminUsername')]",
           "ssh": {
@@ -90,35 +99,10 @@ var azureDeployJSON = `{
           "secret": "[parameters('servicePrincipalClientSecret')]"
         }
       }
-    {{- if not .Volumes}}
     }
-    {{- else}}
-    },
-    {{- end }}
-    {{- $volumeLength := sub (len .Volumes) }}
-    {{- range $i, $volume := .Volumes }}
-    {
-      "apiVersion": "2017-03-30",
-      "type": "Microsoft.Compute/disks",
-      "location": "[resourceGroup().location]",
-      "name": "{{ .Name }}",
-      "properties": {
-        "creationData": {
-          "createOption": "Empty"
-        },
-        "diskSizeGB": {{ .SizeGB }}
-      }
-    {{- if ne $i $volumeLength }}
-    },
-    {{- else }}
-    }
-    {{- end }}
-    {{- end }}
   ]
-}`
-
-// parametersJSON is a raw Go template string used to render azuredeploy.parameters.json
-var parametersJSON = `{
+}`),
+				"azuredeploy.parameters.json": []byte(`{
   "$schema": "http://schema.management.azure.com/schemas/2015-01-01/deploymentParameters.json#",
   "contentVersion": "1.0.0.0",
   "parameters": {
@@ -138,4 +122,30 @@ var parametersJSON = `{
       "value": "GEN-UNIQUE"
     }
   }
-}`
+}`),
+			},
+			nil,
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			files, err := tc.spec.MarshalFiles()
+			if err != tc.err {
+				t.Fatalf("expected error '%v', got '%v'", tc.err, err)
+			}
+			if !reflect.DeepEqual(files, tc.data) {
+				// t.Log("expected:")
+				// for k, v := range tc.data {
+				// 	t.Logf("%s:\n%s\n", k, string(v))
+				// }
+				// t.Log("got:")
+				// for k, v := range files {
+				// 	t.Logf("%s:\n%s\n", k, string(v))
+				// }
+				// TODO: print a diff
+				t.Fatal("data mismatch")
+			}
+		})
+	}
+}
